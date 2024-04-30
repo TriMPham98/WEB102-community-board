97d2c5069ba7a19637adbf2b951c9f57101e3dc5
Tri <trimpham98@gmail.com>
2024-04-29 23:46:23 -0700

    Remove button

--- community-board/src/App.jsx
+++ community-board/src/App.jsx
@@ -17,11 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <div className="card"></div>
     </>
   );
 }
