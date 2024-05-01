867b5f9966542ed80052e92f5ade7113fced4233
Tri <trimpham98@gmail.com>
2024-04-30 22:15:41 -0700

    Remove h1 tag

--- community-board/src/App.jsx
+++ community-board/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card"></div>
     </>
   );
